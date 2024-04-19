3294dacf509d6559b2c047a502b361f32902ae4d
JMO3279 <88757446+JMO3279@users.noreply.github.com>
2024-04-19 17:16:45 +0700

    Create laosisthai.is-an.app.js

---
+++ domains/laosisthai.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'a personal website for my project',
+  domain: 'is-an.app',
+  subdomain: 'laosisthai',
+  owner: {
+    repo: 'https://github.com/JMO3279/Laos-is-Thai',
+    email: 'monspoons@gmail.com',
+  },
+  record: {
+    CNAME: 'https://jmo3279.github.io/',
+  },
+})
